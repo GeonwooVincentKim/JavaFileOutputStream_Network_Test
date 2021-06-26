01e6c9082c43b9ef5be1e5957fcd650f18c9f7fb
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-26 15:56:21 +0900

    [Feature] - Get Java InetAddress

--- src/Network/InetAddressExample.java
+++ src/Network/InetAddressExample.java
@@ -1,5 +1,31 @@
 package Network;
 
-public class InetAddressExample {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import static java.lang.System.out;
+
+public class InetAddressExample {
+    public static void main(String[] args) {
+        try {
+            out.println("접속하고자 하는 사이트의 IP 주소");
+            InetAddress ip = InetAddress.getByName("www.google.co.kr");
+            out.println(ip);
+
+            out.println();
+            out.println("-----------------------------------------");
+            out.println();
+
+            out.println("내 컴퓨터의 IP 주소");
+
+            InetAddress getCurrentIPAddress = InetAddress.getLocalHost();
+            out.println(getCurrentIPAddress.getHostName());
+            out.println(getCurrentIPAddress.getHostAddress());
+        } catch (UnknownHostException e) {
+            out.println("알 수 없는 호스트입니다.");
+            out.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

8703ad16d8b24160e7c11a3cf0fe1ad520ecce84
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-27 12:37:47 +0900

    [Feature] - Print URL Info

--- src/Network/URL/URLExample.java
+++ src/Network/URL/URLExample.java
@@ -7,16 +7,29 @@ import java.net.URL;
 public class URLExample {
     public static void main(String[] args) {
         try {
+            out.println("No URL");
             URL url = null;
             out.println(url);
+
+            out.println();
+            out.println("-----------------------------------------");
+            out.println("Has URL");
 
             url = new URL("https://www.egovframe.go.kr/EgovIntro.jsp?menu=1&submenu=1");
             out.println(url);
 
+            out.println();
+            out.println("-----------------------------------------");
+            out.println("Show URL Info");
+
             out.println("Authority : " + url.getAuthority());
             out.println("Content : " + url.getContent());
             out.println("Protocal : " + url.getProtocol());
-            out.println();
+            out.println("Host : " + url.getHost());
+            out.println("Port : " + url.getPort());
+            out.println("Path : " + url.getPath());
+            out.println("File : " + url.getFile());
+            out.println("Query : " + url.getQuery());
         } catch (Exception e) {
             e.printStackTrace();
         }

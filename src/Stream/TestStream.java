65576d66a8d5d2a641954ae8998074227adb6570
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-04 17:13:13 +0900

    [Feature] - Delete unnecessary codes

--- src/Stream/TestStream.java
+++ src/Stream/TestStream.java
@@ -16,20 +16,6 @@ public class TestStream {
     // 단, 랜덤 값의 범위는 10 ~ 50
     public static List<Integer> calculator(int numberLength) {
         List<Integer> list = new ArrayList<>();
-        // int[] array = new int[numberLength];
-        // for (int i = 0; i < numberLength; i++) {
-        // list.add((int) (Math.random() * (50 - 10 + 1) + 10));
-        // }
-        // IntStream stream = Arrays.stream(array);
-        // IntStream intStream = new Random(numberLength);
-        // Integer getNumberLength = Integer.valueOf(numberLength);
-        // list.stream().forEach(getValue -> list.add((int) (Math.random() * (50 - 10 +
-        // 1)) + 10));
-        // IntStream stream = list.stream().flatMap(data -> {
-
-        // });
-        // Stream<Integer> stream = list.stream();
-        // list
 
         // IntStream 의 길이를 numberLength 만큼 초기화 시킨 후, numberLength 의 길이만큼의 초기화된 intStream
         // 만큼
@@ -40,18 +26,8 @@ public class TestStream {
         // 초기화하지 않은 IntStream 에 값을 집어넣으면 값이 추가되지 않고,
         // 설령 추가되었다고 하더라고 모두 값이 들어가지 않은 완전 빈 값이 들어가게 된다.
         // 결국 추가가 안된다.
-        IntStream.range(0, list.size()).forEach(a -> {
-            list.add((int) (Math.random() * (50 - 10 + 1) + 10));
-        });
-
-        // list.forEach(a -> {
-        // IntStream.range(0, getNumberLength).forEach(j -> {
+        // IntStream.range(0, list.size()).forEach(a -> {
         // list.add((int) (Math.random() * (50 - 10 + 1) + 10));
-        // out.print(list + " ");
-        // });
-        // });
-        // list.stream().forEach(getNumberLength -> {
-        // out.print(list + " ");
         // });
 
         return list;
@@ -73,7 +49,7 @@ public class TestStream {
 
         List<Integer> calculateArray = calculator(userInputLength);
         // out.println(calculateArray);
-        out.println();
+        // out.println();
 
         String result = printResult(calculateArray);
         out.println(result);
